cebc2af45a74bc5a52b0118c86cb50cfe9615f8f
Stefano Pacella <47741309+StefanPacella@users.noreply.github.com>
2024-04-21 11:29:28 +0200

    Update AlgorithmGraphColoring.java
    better performance

--- src/algorithm/AlgorithmGraphColoring.java
+++ src/algorithm/AlgorithmGraphColoring.java
@@ -14,7 +14,6 @@ public class AlgorithmGraphColoring {
 
 	private Map<Integer, Integer> mapNodesChromaticColor = new HashMap<Integer, Integer>();
 	private Graph graph;
-	private List<Integer> stack = new ArrayList<Integer>();
 	private Integer nColor = 1;
 
 	public AlgorithmGraphColoring(Graph graph) {
@@ -27,46 +26,17 @@ public class AlgorithmGraphColoring {
 
 	private void colorTheGraph() {
 		List<Integer> listnode = graph.getListNodes().stream().collect(Collectors.toList());
-		while (!listnode.isEmpty()) {
-			List<Integer> ln = listnode.stream().toList();
-			Set<Integer> colored = colorTheNodes(ln);
-			listnode.removeAll(colored);
+
+		listnode = listnode.stream().sorted(Comparator.comparing(x -> -graph.getListNeighbors(x).size()))
+				.collect(Collectors.toList());
+
+		int limit = listnode.size();
+		for (int i = 0; i < limit; i++) {
+			Integer n = listnode.getFirst();
+			listnode.remove(n);
+			colorTheNode(n);
 		}
-	}
 
-	private Set<Integer> colorTheNodes(List<Integer> listnode) {
-		Set<Integer> colored = new HashSet<Integer>();
-
-		Integer node = getNodeWithMoreNeighbors(listnode);
-		colored.add(node);
-
-		mapNodesChromaticColor.put(node, 1);
-		stack.addAll(graph.getListNeighbors(node));
-
-		while (!stack.isEmpty()) {
-			Integer size = stack.size();
-			Set<Integer> temp = new HashSet<Integer>();
-			for (Integer i = 0; i < size; i++) {
-				Integer nc = getNodeWithMoreNeighbors(stack);
-				colorTheNode(nc);
-				stack.remove(nc);
-				colored.add(nc);
-				temp.add(nc);
-			}
-			for (Integer nc : temp) {
-				stack.addAll(addNeighborsInTheStack(nc));
-			}
-		}
-		return colored;
-	}
-
-	private Set<Integer> addNeighborsInTheStack(Integer node) {
-		Set<Integer> stack = new HashSet<Integer>();
-		for (Integer n : graph.getListNeighbors(node)) {
-			if (!mapNodesChromaticColor.containsKey(n))
-				stack.add(n);
-		}
-		return stack;
 	}
 
 	private void colorTheNode(Integer node) {
@@ -79,33 +49,22 @@ public class AlgorithmGraphColoring {
 	}
 
 	private Integer findTheColor(Integer node) {
+		List<Integer> colorN = new ArrayList<Integer>();
+		for (Integer n : graph.getListNeighbors(node)) {
+			if (mapNodesChromaticColor.containsKey(n))
+				colorN.add(mapNodesChromaticColor.get(n));
+		}
+
 		for (Integer color = 1; color <= nColor; color++) {
-			if (itIsColorable(color, graph.getListNeighbors(node))) {
+			if (!colorN.contains(color)) {
 				return color;
 			}
 		}
 		return nColor + 1;
 	}
 
-	private boolean itIsColorable(Integer color, Set<Integer> nodes) {
-		for (Integer n : nodes) {
-			if (mapNodesChromaticColor.containsKey(n) && mapNodesChromaticColor.get(n).equals(color))
-				return false;
-		}
-		return true;
-	}
-
-	private Integer getNodeWithMoreNeighbors(List<Integer> listNodes) {
-		Integer i = listNodes.stream().findAny().get();
-		for (Integer node : listNodes) {
-			if (graph.getListNeighbors(i).size() < graph.getListNeighbors(node).size())
-				i = node;
-		}
-		return i;
-	}
 
 	public Map<Integer, Integer> getMap() {
 		return mapNodesChromaticColor;
 	}
-
 }
